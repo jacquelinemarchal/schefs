eda7b1c2c964de61e7fdf0f4b51f0001e0bc2446
jacquelinemarchal <64337067+jacquelinemarchal@users.noreply.github.com>
2020-11-17 18:44:25 -0800

    stupid errors on eventBuilder

--- src/pages/eventbuilder.js
+++ src/pages/eventbuilder.js
@@ -1,37 +1,58 @@
 import WhitePillButton from "../components/Buttons/wpillbutton"
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios"
+import ContentEditable from 'react-contenteditable'
 
-export default function OMA () {
-    const [topic, setTopic]  = useState("");
+export default function EventBuilder () {
 
     useEffect(() => {
-
+        // get data
         return()=>(console.log("cleanup"))
     }, [])
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        let sendTopic = {
-            user_id: 1, // ADD AUTH
-            body:topic,
-        }
-        axios.post("http://localhost:5000/api/openmind", sendTopic)
-        .then((res)=>{
-            setTopic("")
-            getTopics(-1)
-        })
-        .catch((err)=>{alert(err)})
-    }
+    const eventName = useRef("");
+    const [charCounter, setCharCounter] = useState("0/65 characters")
 
+    const onBlur = () => {
+       // console.log(eventName.current);
+    };
+
+    const countChars = () => {
+        var maxLength = 65;
+        var strLength = eventName.current.length;
+        console.log(eventName.current)
+        if (strLength > maxLength){
+            setCharCounter(`${strLength} / 65 characters`)
+            // alert to error
+            // set contenteditable to disabled
+        }
+        else{
+            setCharCounter(`${strLength} / 65 characters`)
+        }
+    }
     return (
         <>  
-            <div className="">
+            <div className="grid grid-cols-5 px-6 mx-1">
+                <div className="grid col-span-3 bg-pink-300">
+                    <ContentEditable
+                        html={eventName.current}
+                        onBlur={onBlur}
+                        onKeyUp={countChars}
+                        onChange={(e) => {eventName.current=e.target.value}} 
+                        placeholder={"My event title..."}
+                        className="text-5xl leading-snug mb-2 focus:outline-none"
+                    />
+                    <div className="text-gray-600">
+                        {charCounter}
+                    </div>
+                </div>
+                <div className="grid col-span-2 bg-red-500">
+                    <div className="flex space-x-2 h-8">
+                        <WhitePillButton type="submit" text="SET DATE &#038; SUBMIT" padding="px-6"/>
+                        <WhitePillButton type="submit" text="HELP" padding="px-6"/>
+                    </div>
 
-                <form className="flex row-span-1 items-end justify-center" onSubmit={handleSubmit}>
-                        <input className="w-1/3 pt-4 border-b border-black focus:outline-none" type="text" value={topic} onChange={(e) => setTopic(e.target.value)} aria-label="Add a topic"  />
-                        <WhitePillButton padding="px-4" type="submit" text="POST" size="lg" />
-                </form>
+                </div>
 
             </div>
         </>
