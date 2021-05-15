4e216392e4a11a9f93c424e2fa5e0451d873169c
chriswang030 <cyw2124@columbia.edu>
2021-05-15 13:18:21 -0400

    getEvent query updated to include host emails; remove host emails in GET event endpoint

--- src/utils/queries/events.js
+++ src/utils/queries/events.js
@@ -118,7 +118,8 @@ const getEvent = `
                     u.bio,
                     u.school,
                     u.major,
-                    u.grad_year
+                    u.grad_year,
+                    u.email
                 FROM users AS u
                 LEFT JOIN event_hosts AS eh
                     ON u.uid = eh.user_id
